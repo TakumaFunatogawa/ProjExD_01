ebb64e874e06e5801a0b452d01f0eb18f73a8bf6
c0b20138 <c0b20138a2@edu.teu.ac.jp>
2023-04-18 16:05:26 +0900

    練習問題6

--- pygame_image.py
+++ pygame_image.py
@@ -19,6 +19,7 @@ def main():
         x = tmr%1600
         tmr += 1
         screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [1600-x, 0])
         screen.blit(kk_imgs[tmr%2], [300, 200])
 
         pg.display.update()
